4816c938a70ed04703a442b54de39079988d8764
crais01 <ccortesj.01@gmail.com>
2019-10-28 23:26:59 -0300

    formulario login

---
+++ view/login/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form id="login" name="login" action="#" method="post">
+        <label for="user">Usuario</label>
+        <input type="text" id="user" name="user" require>
+        <label for="password">Cobntraseña</label>
+        <input type="password" id="password" name="passwordr" require>
+        <input type="button" onclick="" value="inicio sesion">
+    </form>
+</body>
+</html>
